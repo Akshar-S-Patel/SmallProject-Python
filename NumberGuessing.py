0d9902f0792b236006533ffd53f48d04e6739797
Aksha Patel <33005706+Akshar-S-Patel@users.noreply.github.com>
2019-06-04 16:02:54 +0530

    Add files via upload

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -1,10 +1,12 @@
-random_number = 18
+import  random
+
+random_number = random.randint(1, 101)
 life = 10
 
 print("Maximum Life = 10")
 while life:
     print("Life :",life)
-    val = int(input("\nEnter a number : "))
+    val = int(input("\nEnter a number (1 - 100): "))
     if val > random_number:
         print("You Guess Too Large")
         life -= 1
